8f67fd9c54bc318a808a8f1f042c6b86de3bf6af
Daniel D <dalydum@yahoo.com>
2021-12-13 17:17:23 +0200

    tema

---
+++ recapitulare.js
@@ -0,0 +1,71 @@
+//let adunareSiImpartire = function (a, b, c) {
+//let rezultatAdunare = a + b;
+//let rezultatImpartire = rezultatAdunare / c;
+//return rezultatImpartire;
+//};
+
+//let adunareSiImpartire2 = function (a, b, c) {
+//return (a + b) / c;
+//};
+
+//console.log(adunareSiImpartire(1, 2, 3));
+//console.log(adunareSiImpartire2(1, 2, 3));
+
+//for (let i = 1; i <= 10; i = i + 1) {
+// console.log('un text' + i);
+//}
+
+//for (let i = 11; i <= 50; i = i + 3) {
+// console.log("un text " + i);
+//}
+
+//for (let i = 100; i >= 50; i = i - 3) {
+//console.log("un text " + i);
+//}
+
+//let exp = 100 >= 50;
+//console.log(exp)
+
+//let exp = 100 >= 500;
+//console.log(typeof exp)
+
+//if (100 >= 50) {
+//console.log('100 is bigger than 50');
+
+//}
+
+//if (100 >= 500) {
+//console.log('100 is bigger than 500');
+//} else {
+//console.log('100 is NOT bigger than 500');
+//}
+//if (100 >= 50) {
+//console.log('100 is bigger than 50');
+//}
+//if (true) {
+//console.log('100 is bigger than 50');
+//}
+
+//let a = 1;
+//let b = 2;
+
+//if (a < b) {
+    //console.log('the condition is true');
+//} else {
+    //console.log('the condition is false');
+//}
+
+
+//let a = {
+    //a: 3,
+    //b: 4,
+    //c: "ana"
+//};
+//let b = [
+    //"a",
+    //"b",
+    //"c"
+//];
+//console.log(a.a);
+
+
---
+++ tema4.js
@@ -0,0 +1,29 @@
+let persoana = 'Jane Smith';
+let persoana = 'name';
+let sum = '$12,450';
+let variabila = 'Toyota Yaris';
+let variabila = 'car';
+let activity1 = 'plans';
+let activity2 = 'to buy';
+let activity3 = has;
+let premise = 'in her banck account';
+let activity4 = 'is';
+let premise1 = 'a lot of money';
+let intention = 'will use her';
+console.log('name + activity3 + sum + premise');
+console.log('sum + activity4 + premise1');
+console.log('name + activity1 + activity2 + car');
+console.log('sum + activity4 + premise1 + car');
+console.log("\n=============================\n")
+console.log('sum + activity4 + premise1');
+console.log('sum + activity 4+ premise1+car');
+console.log('name + activity1 + activity2 + car');
+console.log('name + activity3+ sum + premise');
+console.log('name + intention + sum + activity2 + car');
+console.log("\n=============================\n")
+console.log('name + activity1 + activity2 + car');
+console.log('sum + activity4 + premise1+car');
+console.log('name + intention + sum + activity2 + car');
+onsole.log('name + activity3+ sum + premise');
+console.log('sum + activity4 + premise1');
+
