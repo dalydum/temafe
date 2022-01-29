7a093e2f5c45992562767e0b616600e39e69ffb5
dalydum <dan.ionut.dumitru@gmail.com>
2022-01-29 11:49:18 +0200

    .mmmm

--- tema5.js
+++ tema5.js
@@ -1,27 +1,31 @@
-popup.style.display = 'none';
+let closeButton = document.querySelector('#popupParent .headerClose');
 
+closeButton.addEventListener('click', function() {
 
-// moving the h1 content
-var x = document.getElementById("h1").innerHTML;
-document.getElementById("h11").innerHTML = x;
-var a = document.getElementById("h1").innerHTML;
-document.getElementById("h11").innerHTML = a;
+    let popup = document.getElementById('popup');
+    popup.style.display = 'none';
 
-//moving the p1 content
-var b = document.getElementById("p1").innerHTML;
-document.getElementById("p11").innerHTML = b;
+    // moving the h1 content
+    var x = document.getElementById("h1").innerHTML;
+    document.getElementById("h11").innerHTML = x;
+    var a = document.getElementById("h1").innerHTML;
+    document.getElementById("h11").innerHTML = a;
 
-//moving the p2 content
-var c = document.getElementById("p2").innerHTML;
-document.getElementById("p22").innerHTML = c;
+    //moving the p1 content
+    var b = document.getElementById("p1").innerHTML;
+    document.getElementById("p11").innerHTML = b;
 
-//moving the divstrawberries content
-var d = document.getElementsByClassName("s").innerHTML;
-document.getElementsByClassName("s2").innerHTML = d;
+    //moving the p2 content
+    var c = document.getElementById("p2").innerHTML;
+    document.getElementById("p22").innerHTML = c;
 
-// moving the ullist content
-var e = document.getElementById("ullist").innerHTML;
-document.getElementById("ullist2").innerHTML = e;
+    //moving the divstrawberries content
+    var d = document.getElementsByClassName("s").innerHTML;
+    document.getElementsByClassName("s2").innerHTML = d;
+
+    // moving the ullist content
+    var e = document.getElementById("ullist").innerHTML;
+    document.getElementById("ullist2").innerHTML = e;
 });
 
 let list = document.getElementById("ullist").innerHTML;
