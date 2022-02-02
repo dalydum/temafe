0b1612d2c93b22f8319e183982ef626e52360f22
dalydum <dan.ionut.dumitru@gmail.com>
2022-02-02 11:02:39 +0200

    mutare fisiere

---
+++ domElementsPlay.js
@@ -0,0 +1,20 @@
+//let allOfOurH1s = document.getElementsByTagName('h1');
+
+//let ourSingleH1 = allOfOurH1s[0];
+//console.log(ourSingleH1);
+
+//let myDiv = document.getElementById('header')
+//console.log(myDiv.children[0]);
+
+
+//ourSingleH1.innerHTML += ' (updated)';
+
+//let closeButtons = document.querySelectorAll('#crewPopup .popupHeaderClose');
+
+//let ourcloseButton = closeButtons[0];
+
+//ourcloseButton.addEventListener('click', function () {
+
+//  let ourPopup = document.getElementById('crewPopup');
+//ourPopup.style.display = 'none';
+//});
---
+++ functii.js
@@ -0,0 +1,23 @@
+//function myfunction(clientName) {
+//  console.log('hello ' + clientName + ', I am a first function')
+//return ("Ana")
+//}
+
+//let mySecondFunction = function (clientName) {
+//  console.log('hello ' + clientName + ', I am a second function')
+//}
+//let functionResult = myfunction('Tudor');
+//mySecondFunction("Mihai");
+
+//let threeNumbersSum = function (a, b, c) {
+//  let result = a + b + c;
+//return result;
+//}
+
+//let result1 = 4 + 5 + 6
+//let result2 = threeNumbersSum(4, 5, 6, 7);
+//let result3 = threeNumbersSum(40, 50, 60);
+//let result4 = threeNumbersSum(400, 500, 600);
+//let result5 = threeNumbersSum(4452, 56677, 699);
+//console.log(result1);
+//console.log(result2);
